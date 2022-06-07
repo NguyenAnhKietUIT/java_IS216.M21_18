8ad54281a384fc115f8378f961e5b7895666cdfc
Linh Nguyễn <20521534@gm.uit.edu.vn>
2022-06-07 15:00:00 +0700

    update

--- QuanLyTimViec/src/ConnectDB/ConnectOracle.java
+++ QuanLyTimViec/src/ConnectDB/ConnectOracle.java
@@ -11,7 +11,7 @@ public class ConnectOracle {
     public static Connection openConnection() throws Exception {
         String url = "jdbc:oracle:thin:@localhost:1521:orcl";
         String username = "QuanLyTimViec";
-        String password = "anhkiet22032002";
+        String password = "HoaiLinh2304";
         Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection conn = DriverManager.getConnection(url, username, password);
 
